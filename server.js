abe0369ace1fdddab3c42a64d7421e6d9f42c791
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-07-15 23:26:12 -0400

    add html gets

--- server.js
+++ server.js
@@ -14,11 +14,29 @@ app.use(express.static('public'));
 // Get note and send to db.json
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '/db/db.json'))
-})
+});
 
 // post to add new notes to db.json
 app.post("/api/notes", (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNote = req.body;
     newNote.id = uuid.v4();
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+});
+
+// html
+// for landing page
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// for notes page
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// listen
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT);
 })
